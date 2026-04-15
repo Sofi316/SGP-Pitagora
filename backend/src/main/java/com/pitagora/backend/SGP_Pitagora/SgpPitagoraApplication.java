cdeba2d1b9d1c465b7014eb702ca848bcb5b8af0
Sofi316 <so.hormazabal@duocuc.cl>
2026-04-15 10:36:28 -0400

    Subiendo estructura base de Docker, Spring y React

---
+++ backend/src/main/java/com/pitagora/backend/SGP_Pitagora/SgpPitagoraApplication.java
@@ -0,0 +1,13 @@
+package com.pitagora.backend.SGP_Pitagora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpPitagoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpPitagoraApplication.class, args);
+	}
+
+}
