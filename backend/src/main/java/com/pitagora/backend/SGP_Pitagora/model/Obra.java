afa88a68d5635cbdea1ea833650c9bee0688bffd
Sofi316 <so.hormazabal@duocuc.cl>
2026-04-19 12:31:10 -0400

    Creada carpeta model y los siguientes modelos: region,  comuna,  categoria,  subcategoria, estadoSolicitud,  comunicacionArchivada, tipoEvidencia, archivoEvidencia,  obra, empresaCliente, rol, usuario y solicitud

---
+++ backend/src/main/java/com/pitagora/backend/SGP_Pitagora/model/Obra.java
@@ -0,0 +1,44 @@
+package com.pitagora.backend.SGP_Pitagora.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="obra")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Obra {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_obra")
+    private Long id;
+
+    @Column(name="nombre_obra",nullable = false)
+    private String nombre;
+
+    @Column(name="direccion_obra",nullable = false)
+    private String direccion;
+
+    @Column(name="fecha_inicio_postventa",nullable = false)
+    private LocalDate fechaInicioPostventa;
+
+    @Column(name="fecha_cierre_postventa",nullable = false)
+    private LocalDate fechaCierrePostventa;
+
+    @Column(name="ruta_acta_entrega",columnDefinition="TEXT")
+    private String rutaActaEntrega;
+
+    //private EmpresaCliente empresaCliente;
+}
