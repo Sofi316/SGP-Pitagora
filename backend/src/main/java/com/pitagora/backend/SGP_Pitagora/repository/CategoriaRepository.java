b01df8ae122d635bd844612098b08024709927ef
Sofi316 <so.hormazabal@duocuc.cl>
2026-04-22 14:24:24 -0400

    Creacion de los siguientes repositorios: CategoriaRepository, subCategoriaRepository, comunicacionArchivadaRepository, empresaClienteRepository, ObraRepository

---
+++ backend/src/main/java/com/pitagora/backend/SGP_Pitagora/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.pitagora.backend.SGP_Pitagora.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pitagora.backend.SGP_Pitagora.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
