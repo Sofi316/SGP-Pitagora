89b7b2695b02f34320ba413b92103b09f36f2e33
Sofi316 <so.hormazabal@duocuc.cl>
2026-05-06 12:52:38 -0400

    Creacion y prueba de envio y recepcion de correos

--- producto/codigo-fuente/backend/src/main/java/com/pitagora/backend/SGP_Pitagora/dto/MensajeSalienteDto.java
+++ producto/codigo-fuente/backend/src/main/java/com/pitagora/backend/SGP_Pitagora/dto/MensajeSalienteDto.java
@@ -1,5 +1,14 @@
 package com.pitagora.backend.SGP_Pitagora.dto;
 
-public class MensajeSalienteDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MensajeSalienteDto {
+    private String destinatario;
+    private String asunto;
+    private String cuerpoMensaje;
 }
