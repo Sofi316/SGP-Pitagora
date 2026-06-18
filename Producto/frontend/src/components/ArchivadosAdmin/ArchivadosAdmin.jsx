cbad49048d7c39b8e8322654981606840b2aa7e3
Sofia Antonia Hormazabal Urzua <so.hormazabal@duocuc.cl>
2026-06-18 17:13:21 -0400

    Remove unused Link import in ArchivadosAdmin.jsx
    Removed unused import of Link from react-router-dom.

--- Producto/frontend/src/components/ArchivadosAdmin/ArchivadosAdmin.jsx
+++ Producto/frontend/src/components/ArchivadosAdmin/ArchivadosAdmin.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
-import { Link } from 'react-router-dom';
 import styles from './ArchivadosAdmin.module.css';
 
 const ArchivadosAdmin = () => {
