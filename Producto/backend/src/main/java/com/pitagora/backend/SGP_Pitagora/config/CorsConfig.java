fc3ce46e59b0c4c90ca35c7dfc144837bcfc9185
Sofia Antonia Hormazabal Urzua <so.hormazabal@duocuc.cl>
2026-06-18 17:52:07 -0400

    Add new CORS origin for production environment

--- Producto/backend/src/main/java/com/pitagora/backend/SGP_Pitagora/config/CorsConfig.java
+++ Producto/backend/src/main/java/com/pitagora/backend/SGP_Pitagora/config/CorsConfig.java
@@ -3,13 +3,18 @@ package com.pitagora.backend.SGP_Pitagora.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:3000", 
+                        "http://localhost:5173",
+                        "https://sgp-pitagora-11wssu7zd-sofi316s-projects.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
